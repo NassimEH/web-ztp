ce2e0173122f7a9ea58915bfae557aa95816e69e
NassimEH <nassim.elhaddad2004@gmail.com>
2025-03-24 15:49:17 +0100

    ajout de l'authentification

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from app.models import Device, Template, DHCPConfig
 from django.db import models
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class AddForm(forms.ModelForm):
@@ -70,3 +72,18 @@ class DHCPConfigUpdateForm(UpdateForm):
     class Meta:
         model = DHCPConfig
         fields = ["subnet", "min_ip_pool", "max_ip_pool"]
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
