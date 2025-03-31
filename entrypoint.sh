ee24b9e687e3193c6eef63af7d08774d02ff3ca1
SeyyLii <ilyesb71@gmail.com>
2025-03-31 17:32:34 +0200

    Docker et bdd postgresql fonctionne

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
 
+# Fonction pour attendre que PostgreSQL soit prêt
+postgres_ready() {
+    python << END
+import sys
+import psycopg2
+try:
+    psycopg2.connect(
+        dbname="webztp_db",
+        user="webztp_user",
+        password="webztp_password",
+        host="db"
+    )
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+
+# Attente que PostgreSQL soit prêt
+until postgres_ready; do
+  >&2 echo "PostgreSQL n'est pas disponible - attente..."
+  sleep 1
+done
+>&2 echo "PostgreSQL est prêt !"
+
 echo "Exécution des migrations..."
 python manage.py migrate
 
--- webZtp/settings.py
+++ webZtp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,10 +90,10 @@ WSGI_APPLICATION = 'webZtp.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://webztp_user:webztp_password@localhost:5432/webztp_db',
+        conn_max_age=600
+    )
 }
 
 # Password validation
