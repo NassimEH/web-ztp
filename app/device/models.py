61360e43e2a50a6ac213b972a3f95687e7ac66f8
Chadi-Mangle <chadimangled@gmail.com>
2025-06-10 22:50:50 +0200

    feat: change DHCPConfig to a singleton models

--- app/device/models.py
+++ app/device/models.py
@@ -25,13 +25,29 @@ class Device(models.Model):
     default_gateway = models.GenericIPAddressField(null=True, blank=True)
     login = models.CharField(max_length=255, null=True, blank=True)
     password = models.CharField(max_length=255, null=True, blank=True)
-    
 
     def __str__(self):
         return f"{self.hostname} ({self.ip})"
 
 
 class DHCPConfig(models.Model):
+    """This model is used to store the DHCP server configuration
+    Only one instance of this model should exist in the database (Singleton).
+
+    The save method ensures that only one instance can be created
+    and overwritten the previous one.
+    """
+
     subnet = models.GenericIPAddressField()
     min_ip_pool = models.GenericIPAddressField()
     max_ip_pool = models.GenericIPAddressField()
+
+    def save(self, *args, **kwargs):
+        if self.__class__.objects.exists() and not self.pk:
+            existing = self.__class__.objects.first()
+            self.pk = existing.pk
+        super().save(*args, **kwargs)
+
+    @classmethod
+    def get_instance(cls):
+        return cls.objects.first()
