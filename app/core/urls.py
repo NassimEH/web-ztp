2079b963e2b7b0c0627d3ec00474ed45c5877d26
Chadi-Mangle <chadimangled@gmail.com>
2025-06-23 22:55:54 +0200

    dark mode

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LandingPageView, ConditionalRedirectView, HelpPageView
+from .views import LandingPageView, ConditionalRedirectView, HelpPageView, DashboardView
 
 urlpatterns = [
     path("home/", LandingPageView.as_view(), name="landing_page"),
     path("help/", HelpPageView.as_view(), name="help_page"),
+    path("dashboard/", DashboardView.as_view(), name="dashboard"),
     path("", ConditionalRedirectView.as_view(), name="home"),
 ]
--- app/core/views.py
+++ app/core/views.py
@@ -11,8 +11,12 @@ class HelpPageView(TemplateView):
     template_name = "core/help.html"
 
 
+class DashboardView(TemplateView):
+    template_name = "core/dashboard.html"
+
+
 class ConditionalRedirectView(View):
     def get(self, request):
         if request.user.is_authenticated:
-            return redirect("account_profile")
+            return redirect("dashboard")
         return redirect("landing_page")
