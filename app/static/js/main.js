77e1f615ed242383b71f90e8482de12e0cdef34d
Chadi-Mangle <chadimangled@gmail.com>
2025-06-24 12:17:01 +0200

    fix: navbar with unpoly

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,20 +1,16 @@
-// Mode sombre
 const themeToggle = document.getElementById('theme-toggle');
 const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
 
-// Vérifier le thème stocké ou le préférence système
 const currentTheme = localStorage.getItem('theme') || (prefersDarkScheme.matches ? 'dark' : 'light');
 if (currentTheme === 'dark') {
     document.body.classList.add('dark');
 }
 
-// Mettre à jour l'icône
 if (themeToggle) {
     themeToggle.innerHTML = currentTheme === 'dark' ? 
         '<ion-icon name="sunny-outline"></ion-icon>' : 
         '<ion-icon name="moon-outline"></ion-icon>';
 
-    // Gérer le changement de thème
     themeToggle.onclick = function() {
         if (document.body.classList.contains('dark')) {
             document.body.classList.remove('dark');
