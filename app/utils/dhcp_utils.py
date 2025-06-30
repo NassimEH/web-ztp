d76ae7f6dbe9d59a7189e81ed1c11caea39b5372
SeyyLii <126691467+SeyyLii@users.noreply.github.com>
2025-06-30 14:28:45 +0200

    Create dhcp_utils.py

---
+++ app/utils/dhcp_utils.py
@@ -0,0 +1,7 @@
+from app.models import DHCPConfig
+
+
+def get_dhcp_config():
+    """Récupère les informations du subnet DHCP et du pool d'adresses IP."""
+    config = DHCPConfig.objects.first()
+    return config
