df903c858a15e962e09a948fd74e5b533e8fed0c
NassimEH <nassim.elhaddad2004@gmail.com>
2025-07-07 16:41:25 +0200

    fix d'un bug où la session durait trop peu.

--- app/webZtp/settings.py
+++ app/webZtp/settings.py
@@ -219,3 +219,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "auth.User"
 
 LOGIN_URL = "/login/"
+
+# --- Paramètres de session pour une durée longue ---
+SESSION_COOKIE_AGE = 60 * 60 * 24 * 14  # 2 semaines
+SESSION_SAVE_EVERY_REQUEST = True  # Prolonge la session à chaque requête
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False  # Ne pas supprimer la session à la fermeture du navigateur
